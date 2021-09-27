684c94f7060d23b76cec82dbd38be46688bd760a
malcops <paul.malcolm@protonmail.com>
2021-09-27 19:39:50 -0400

    [UPDATE] build.sh, favicons folder, ffmpeg to reduce img size

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+rm -rf public
+git submodule init
+git submodule update
+hugo
+
+# why are the pictures being rotated??
+for f in $(find public/img -type f \( -iname \*.jpg -o -iname \*.JPG -o -iname \*.PNG -o -iname \*.png \))
+do
+        PICTURE_NAME="reduced-$(basename $f)"
+        PICTURE_PATH=$(dirname $f)
+        ffmpeg -loglevel 2 -i $f -q:v 2 "${PICTURE_PATH}/${PICTURE_NAME}" -y
+        rm -f $f
+done
+
+cp favicons/* public
+
+public_size=$(du -s public/ | cut -f1)
+resources_size=$(du -s resources/ | cut -f1)
+published_size=$(( public_size + resources_size ))
+echo "${published_size}"
+
+max_published_size=300000
+if [ "$published_size" -ge "$max_published_size" ]; then
+    echo "The generated site is too big!"
+fi
